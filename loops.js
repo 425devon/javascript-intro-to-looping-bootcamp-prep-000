0eb62fa4c78c632e579ee8c855753dbbea55da73
Devon Langendoerfer <425devon@gmail.com>
2017-11-15 02:11:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,3 +18,13 @@ function whileLoop(n){
   return 'done';
 }
 
+function doWhileLoop(array){
+  var i = array.length - 1
+  do{
+    array.pop
+    i--
+  }while(i>0){
+    return array;
+  }
+  }
+}
